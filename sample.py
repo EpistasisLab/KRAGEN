373dd0be622ce0de467218bdbeb36a16ceb10e12
Jay Moran <jay-moran@outlook.com>
2024-01-23 15:29:06 -0800

    Update python and pandas version

--- sample.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("test.csv")
-
-unique_types = df["source_label"].unique()
-
-for row_type in unique_types:
-    x = df[df["source_label"]==row_type].head(300)
-    x.to_csv("my_test.csv", mode='a', header=True, index=False)
