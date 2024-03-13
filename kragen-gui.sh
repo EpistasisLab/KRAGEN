115fd181842380389eaf96c47753fd54932fece7
nickotto <nicholas.y.matsumoto@gmail.com>
2024-03-13 13:45:54 -0700

    bash gui update

--- kragen-gui.sh
+++ kragen-gui.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-pip install -r ./KRAGEN_Dashboard/Backend/ExecGPTServer/requirements.txt
-
-
-
 # Copy .env file and replace the value of the 11th line
 cp ./KRAGEN_Dashboard/Backend/config.json.sample ./KRAGEN_Dashboard/Backend/config.json
 
@@ -25,30 +21,21 @@ sed -i "15s/.*/$text/" ./KRAGEN_Dashboard/Backend/config.json
 text="\"url\": \"$weaviateurl\","
 sed -i "16s/.*/$text/" ./KRAGEN_Dashboard/Backend/config.json
 
-# Start Flask server in the background
+# Start Flask server in Docker container
 cd ./KRAGEN_Dashboard/Backend
-pip install -e graph_of_thoughts
-flask --app ExecGPTServer init-db
-python execgpt.py > flask_server.log 2>&1 &
-FLASK_PID=$!
-
-# Function to trap exit signals and kill subprocesses
-function cleanup_flask() {
-    echo "Exiting..."
-    kill $FLASK_PID
-    exit
-}
-
-# Trap exit signals
-trap cleanup_flask EXIT
+docker build -t kragen-flask-server .
+docker run -d -p 5050:5050 kragen-flask-server 
 
 # Start React app in the background
 cd ../Frontend
+
+cp .env.sample .env
+
 npm install
 npm start &
 REACT_PID=$!
 
-# Function to trap exit signals and kill subprocesses
+# Function to trap exit signals and kill subprocessesN
 function cleanup_react() {
     echo "Exiting..."
     kill $REACT_PID
