3c63f630b51c5b152a566ac85c5f566324c28852
nickotto <nicholas.y.matsumoto@gmail.com>
2024-03-13 11:51:38 -0700

    update requirements and turn off debug

--- KRAGEN_Dashboard/Backend/execgpt.py
+++ KRAGEN_Dashboard/Backend/execgpt.py
@@ -6,10 +6,10 @@ def main():
     args = ["flask", "--app", "ExecGPTServer", "run", "--port", "5050"]
     arguments = sys.argv[1:]
 
-    if len(arguments) > 0 and arguments[0] == "debug":
-        args.append("--debug")
-    # temporarily default to debug
-    args.append("--debug")
+    # if len(arguments) > 0 and arguments[0] == "debug":
+    #     args.append("--debug")
+    # # temporarily default to debug
+    # args.append("--debug")
 
     subprocess.call(args)
 
