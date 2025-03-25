169d94a1f2365181f4d9f84cc5f2bf04e56b2f13
nickotto <ichplaymates@gmail.com>
2025-03-25 15:17:21 -0700

    config file

---
+++ KRAGEN/config.py
@@ -0,0 +1,18 @@
+config = {
+    "azuregpt-4o-mini" : {
+        "model_id":"gpt-4o-mini", 
+        "prompt_token_cost": 0.001,
+        "response_token_cost": 0.002,
+        "temperature": 0.7,
+        "max_tokens": 4096,
+        "stop": None,
+        "api_version": "",
+        "api_base": "",
+        "api_key": "",
+        "embedding_id":"text-embedding-ada-002"
+    },
+    "memgraph" : {
+        "host": "alzkb.ai",
+        "port": 7687
+    }
+}
